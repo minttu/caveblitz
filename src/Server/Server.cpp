791c5352ee4b1ac5c02749a73566384c8549359e
Minttu Imberg <minttu@imberg.fi>
2017-11-19 12:16:07 +0200

    Stop match on last peer disconnecting (closes #4)

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -6,7 +6,7 @@ Server::Server(uint16_t port)
     this->update_data->reserve(1024);
     this->input_data->reserve(1024);
 
-    this->match = std::make_shared<MatchServer>(MatchServer());
+    this->match = nullptr;
 
     if (enet_initialize() != 0) {
         throw std::runtime_error("enet_initialize() failed");
@@ -22,10 +22,11 @@ Server::Server(uint16_t port)
         throw std::runtime_error("enet_host_create() failed");
     }
 
-    std::cerr << "server listening on 0.0.0.0:" << std::to_string(port) << "\n";
+    std::cerr << "SERVER listening on 0.0.0.0:" << std::to_string(port) << "\n";
 }
 
 void Server::next_match() {
+    std::cerr << "SERVER Loading match\n";
     this->match = std::make_shared<MatchServer>(MatchServer());
     this->match_version++;
 }
@@ -40,9 +41,10 @@ void Server::run(const bool *should_run) {
     uint32_t ticks = 0;
     std::vector<PlayerID> joined_players;
     MatchReset match_reset;
+    int connected_peers = 0;
 
     while (*should_run) {
-        if (match->match_status == MATCH_ENDED) {
+        if (match != nullptr && match->match_status == MATCH_ENDED) {
             this->next_match();
         }
 
@@ -50,11 +52,21 @@ void Server::run(const bool *should_run) {
             auto peer_info = static_cast<PeerInfo *>(event.peer->data);
             switch (event.type) {
             case ENET_EVENT_TYPE_CONNECT:
+                if (connected_peers == 0) {
+                    std::cerr << "SERVER First peer connected, starting match\n";
+                    this->next_match();
+                }
                 event.peer->data = new PeerInfo{this->match_version};
+                connected_peers++;
                 break;
             case ENET_EVENT_TYPE_DISCONNECT:
                 delete static_cast<PeerInfo *>(event.peer->data);
                 event.peer->data = nullptr;
+                connected_peers--;
+                if (connected_peers == 0) {
+                    std::cerr << "SERVER Last peer disconnected, stopping match\n";
+                    this->match = nullptr;
+                }
                 break;
             case ENET_EVENT_TYPE_RECEIVE:
                 if (peer_info->match_version != this->match_version) {
@@ -96,25 +108,25 @@ void Server::run(const bool *should_run) {
             }
         }
 
-        // update
-        this->match->update(dt);
-        this->update_data->clear();
-        this->match->serialize(this->update_data);
-        ENetPacket *update_packet =
-                enet_packet_create(this->update_data->data(), this->update_data->size(), 0);
-        enet_host_broadcast(this->server, 0, update_packet);
+        if (this->match != nullptr) {
+            this->match->update(dt);
+            this->update_data->clear();
+            this->match->serialize(this->update_data);
+            ENetPacket *update_packet =
+                    enet_packet_create(this->update_data->data(), this->update_data->size(), 0);
+            enet_host_broadcast(this->server, 0, update_packet);
 
-        this->update_data->clear();
-        this->match->serialize_reliable(this->update_data);
-        if (!this->update_data->empty()) {
-            update_packet = enet_packet_create(this->update_data->data(),
-                                               this->update_data->size(),
-                                               ENET_PACKET_FLAG_RELIABLE);
-            enet_host_broadcast(this->server, 1, update_packet);
+            this->update_data->clear();
+            this->match->serialize_reliable(this->update_data);
+            if (!this->update_data->empty()) {
+                update_packet = enet_packet_create(this->update_data->data(),
+                                                   this->update_data->size(),
+                                                   ENET_PACKET_FLAG_RELIABLE);
+                enet_host_broadcast(this->server, 1, update_packet);
+            }
+
+            enet_host_flush(this->server);
         }
-
-        // send
-        enet_host_flush(this->server);
 
         // sleep
         ticks++;
@@ -129,11 +141,13 @@ void Server::run(const bool *should_run) {
         if (current_time <= target_time) {
             std::this_thread::sleep_for(target_time - current_time);
         } else {
-            std::cerr << "server no sleep\n";
+            std::cerr << "SERVER no sleep\n";
             ticks = 0;
             start_time = last_time;
         }
     }
+
+    std::cerr << "SERVER stopping\n";
 }
 
 Server::~Server() {
